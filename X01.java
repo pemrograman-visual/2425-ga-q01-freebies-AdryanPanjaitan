4e041485e8d51ff6eacdf1e1b7946f9e72c6fd22
AdryanPanjaitan <andryanpanjaitan30@gmail.com>
2024-10-09 16:36:22 +0700

    Pair 16 selesai

--- X01.java
+++ X01.java
@@ -1,2 +1,38 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24008 - Rospika Sarah Yosefin Siregar
+// 12S24013 - Adryan Julianto Panjaitan
+
+import java.util.*;
+import java.lang.Math;
+
+public class X01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double hargabuku, hargasementara, hargaterkecil, totalharga;
+
+        hargaterkecil = 100;
+        hargabuku = 0;
+        hargasementara = 0;
+        do {
+            hargabuku = Double.parseDouble(input.nextLine());
+            if (hargabuku == 0) {
+            } else {
+                if (hargaterkecil < hargabuku) {
+                } else {
+                    hargaterkecil = hargabuku;
+                }
+                hargasementara = hargabuku + hargasementara;
+            }
+        } while (hargabuku != 0);
+        if (hargasementara >= 100) {
+            totalharga = hargasementara - hargaterkecil;
+        } else {
+            totalharga = hargasementara;
+        }
+        System.out.println(toFixed(totalharga,2));
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
